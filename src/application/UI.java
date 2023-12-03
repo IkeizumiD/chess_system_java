959fecbe42116bc0ab7e38d602b538d3f437cea3
IkeizumiD <Ikeizumi_vinicius@Hotmail.com>
2023-12-03 12:03:48 -0300

    Fix - better treatment to promotion

--- src/application/UI.java
+++ src/application/UI.java
@@ -36,7 +36,7 @@ public class UI {
 		System.out.print("\033[H\033[2J");
 		System.out.flush();
 	}
-	
+
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
 			String s = sc.nextLine();
@@ -59,12 +59,12 @@ public class UI {
 			if (chessMatch.getCheck()) {
 				System.out.println("CHECK!");
 			}
-		}
-		else {
+		} else {
 			System.out.println("CHECKMATE!");
 			System.out.println("Winner: " + chessMatch.getCurrentPlayer());
 		}
 	}
+
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
@@ -86,7 +86,7 @@ public class UI {
 		}
 		System.out.println("  a b c d e f g h");
 	}
-	
+
 	private static void printPiece(ChessPiece piece, boolean background) {
 		if (background) {
 			System.out.print(ANSI_BLUE_BACKGROUND);
@@ -102,10 +102,12 @@ public class UI {
 		}
 		System.out.print(" ");
 	}
-	
+
 	private static void printCapturedPieces(List<ChessPiece> captured) {
-		List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
-		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
+		List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE)
+				.collect(Collectors.toList());
+		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK)
+				.collect(Collectors.toList());
 		System.out.println("Captured pieces: ");
 		System.out.print("White: ");
 		System.out.print(ANSI_WHITE);
@@ -114,6 +116,6 @@ public class UI {
 		System.out.print("Black: ");
 		System.out.print(ANSI_YELLOW);
 		System.out.println(Arrays.toString(black.toArray()));
-		System.out.println(ANSI_RESET);	
-	}	
+		System.out.println(ANSI_RESET);
+	}
 }
--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -1,12 +1,11 @@
 package boardgame;
 
 public class Position {
-	
+
 	private int row;
 	private int column;
-	
-	
-	public Position(int row, int column) {	
+
+	public Position(int row, int column) {
 		this.row = row;
 		this.column = column;
 	}
@@ -15,26 +14,23 @@ public class Position {
 		return row;
 	}
 
-
 	public void setRow(int row) {
 		this.row = row;
 	}
-
 
 	public int getColumn() {
 		return column;
 	}
 
-
 	public void setColumn(int column) {
 		this.column = column;
 	}
-	
+
 	public void setValues(int row, int column) {
 		this.row = row;
 		this.column = column;
 	}
-	
+
 	@Override
 	public String toString() {
 		return row + ", " + column;
